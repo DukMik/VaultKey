db6c9c6c2aee1c8f2532f2fe4333ae3841b11746
DukMik <dukmik.game@gmail.com>
2025-04-08 22:20:39 +0200

    add blazor but not done

---
+++ TheBlazorVault/Program.cs
@@ -0,0 +1,94 @@
+/*using MudBlazor.Services;
+using TheBlazorVault.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMudServices();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();*/
+
+using TheBlazorVault.Components;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Identity.Web;
+using MudBlazor.Services;
+using TheBlazorVault.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+// Ajouter l'authentification JWT
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(builder.Configuration, "AzureAd");
+
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+//Lecture des informations de la WebAPI depuis le fichier de configuration.
+string apiEndpoint = builder.Configuration.GetValue<string>("WebAPI:Endpoint") ?? throw new InvalidOperationException("WebAPI is not configured");
+string apiScope = builder.Configuration.GetValue<string>("WebAPI:Scope") ?? throw new InvalidOperationException("WebAPI is not configured");
+
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(builder.Configuration, "AzureAd")
+    .EnableTokenAcquisitionToCallDownstreamApi([apiScope])
+    .AddDownstreamApi("EntraIDAuthWebAPI", options =>
+    {
+        options.BaseUrl = apiEndpoint;
+        options.Scopes = [apiScope];
+    })
+    .AddInMemoryTokenCaches();
+
+builder.Services.AddAuthorization();
+builder.Services.AddCascadingAuthenticationState();
+
+//builder.Services.AddScoped<VaultService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles(); // 🔹 nécessaire pour charger les fichiers wwwroot
+
+app.UseRouting();     // 🔹 nécessaire pour activer le routing
+app.UseAuthentication(); // 🔹 nécessaire pour l'auth
+app.UseAuthorization();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
