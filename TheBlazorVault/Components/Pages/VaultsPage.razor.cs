5c6800e6f5c276c69aadd845ff7636e0d9b153fc
DukMik <dukmik.game@gmail.com>
2025-04-13 13:11:03 +0200

    il manqais les pages

---
+++ TheBlazorVault/Components/Pages/VaultsPage.razor.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+using TheApiDto;
+
+
+
+namespace TheBlazorVault.Components.Pages
+{
+    public partial class VaultsPage
+    {
+        [Parameter]
+        public Guid entraIdUser { get; set; }
+        public List<VaultDto> _vaults { get; set; } = new ();
+       
+        private Guid _entraIdUser = Guid.Parse("a64e2dda-b127-4219-8225-7ea10fdc0e0e") ;
+        protected override async Task OnInitializedAsync()
+        {
+            var authState = await AuthStateProvider.GetAuthenticationStateAsync();
+            var user = authState.User;
+            if (user.Identity.IsAuthenticated)
+            {
+                // Vous pouvez logger ou afficher le nom de l'utilisateur par exemple
+                Console.WriteLine("Utilisateur authentifié : " + user.Identity.Name);
+            }
+            else
+            {
+                NavigationManager.NavigateTo("authentication/login");
+            }
+            
+            
+            // Appel du service pour récupérer la liste des vaults de l'utilisateur connecté
+            _vaults = await CallServices.GetVaultsAsync(_entraIdUser);
+                
+        
+        }
+    }
+
+    
+}
+
